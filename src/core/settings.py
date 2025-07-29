2f9843420b065615fb79f41551fa1f17d58e006a
ottsupi <ott.regisorate@gmail.com>
2025-07-29 04:56:26 +0000

    feat: use argon2 for password hash

--- src/core/settings.py
+++ src/core/settings.py
@@ -110,6 +110,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Password management
+# https://docs.djangoproject.com/en/5.2/topics/auth/passwords/
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 

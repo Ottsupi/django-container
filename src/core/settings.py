e94ab4d982eaad1c4b03e62c86e94faa7258ea45
ottsupi <ott.regisorate@gmail.com>
2025-07-29 04:55:20 +0000

    refactor: change django settings to take from environment variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!x_n@grfto-_(wkt8(ba^ddsye&hjfwdn=!-f#-@qs361p6)uw"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
 
 # Application definition

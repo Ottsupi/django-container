a501ad969af0612cd4d5743069fcc080f0982a5b
ottsupi <ott.regisorate@gmail.com>
2025-08-15 18:42:32 +0800

    ops: add flags to local deployment script

--- release.local.sh
+++ release.local.sh
@@ -14,6 +14,59 @@ else
     exit 1
 fi
 
+# If no options given, display help
+if (( $# == 0 )); then
+    echo "This is the deployment script for..."
+    echo "Project:     $PROJECT_NAME"
+    echo "Environment: $ENVIRONMENT"
+    echo "Directory:   $BASE_PATH"
+    echo "Branch:      $(git rev-parse --abbrev-ref HEAD)"
+    echo ""
+    echo "Usage:"
+    echo "./$(basename $0) [options]"
+    echo ""
+    echo "Options:"
+    echo "--deploy     This must be the first option given to allow"
+    echo "               running the deployment script."
+    echo "--initial    Specifies initial deployment"
+    echo "--no-code    Allows the script to run even without code changes."
+    echo "               Use in the initial deployment."
+    exit 0
+fi
+
+if [ "$1" != "--deploy" ]; then
+    echo "The first option must be '--deploy' to run the script"
+    echo "Run './$(basename $0)' to view the help text"
+    exit 1
+fi
+
+FLAG_INITIAL=0
+FLAG_NO_CODE=0
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --deploy)
+            echo "Deployment script started"
+            ;;
+        --initial)
+            echo "Initial deployment"
+            FLAG_INITIAL=1
+            ;;
+        --no-code)
+            echo "Deploy without code change"
+            FLAG_NO_CODE=1
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
+echo ""
+
 echo "*** Pre-release Script ***"
 echo "Project:     $PROJECT_NAME"
 echo "Environment: $ENVIRONMENT"
@@ -113,7 +166,12 @@ echo ""
 echo "Attempting to backup database..."
 echo ""
 
-./scripts/local.db-backup.sh
+if [ $FLAG_INITIAL -eq 1 ]; then
+    echo "Initial deployment."
+    echo "Skipping database backup..."
+else
+    ./scripts/local.db-backup.sh
+fi
 
 if [ $? -eq 1 ]; then
     echo "Skipping pre-deploy database backup..."
@@ -133,9 +191,18 @@ if [ "$OLD_GIT_COMMIT_HASH" == "$LATEST_GIT_COMMIT_HASH" ]; then
     echo "Branch:  $GIT_BRANCH"
     echo "Latest:  $LATEST_GIT_COMMIT_HASH"
     echo "Current: $OLD_GIT_COMMIT_HASH"
-    echo "No new changes to deploy"
-    exit 0
+    if [ $FLAG_INITIAL -eq 1 ]; then
+        echo "Allowed to deploy without code changes"
+        echo "No code changes to deploy but deploying anyway"
+    else
+        echo "No new changes to deploy"
+        exit 0
+    fi
 fi
+
+echo ""
+echo "Attempting to deploy..."
+echo ""
 
 echo "*** Release Script ***"
 echo "Project:     $PROJECT_NAME"
@@ -144,7 +211,9 @@ echo "Directory:   $BASE_PATH"
 echo "Branch:      $GIT_BRANCH"
 echo "Commit ID:   $LATEST_GIT_COMMIT_HASH"
 
-echo "Ready for deployment to local!"
+echo ""
+echo "Ready for deployment!"
+echo "Docker will take it from here."
 echo "Starting in..."
 sleep 1
 

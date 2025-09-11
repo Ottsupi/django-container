b8cbe0a1b5d5affd222d1ce1a674611a894815d0
ottsupi <ott.regisorate@gmail.com>
2025-09-11 06:58:29 +0000

    feat: add media files settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -181,6 +181,14 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "assets"]
 
+
+# Media files (user-uploaded files and images)
+# https://docs.djangoproject.com/en/5.2/topics/files/
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

b0616af88b034314d8fdb2423aa4ee29be5641ff
ottsupi <ott.regisorate@gmail.com>
2025-09-11 07:34:50 +0000

    feat: implement serving media files through django on debug mode

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,7 @@ URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +14,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -30,3 +33,4 @@ if settings.DEBUG:
 
     urlpatterns.extend(debug_toolbar_urls())
     urlpatterns += (path("__reload__/", include("django_browser_reload.urls")),)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
